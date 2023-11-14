9f59b8cc5e52b0d63b235a59d66205e07c949072
shaneOuO <shanecheng2002@gmail.com>
2023-11-15 00:50:38 +0800

    upload the second version of menu

--- test.py
+++
@@ -1,5 +0,0 @@
-import create_menu
-
-# create_menu.create_menu('initial_menu','C:/Users/USER/Desktop/python/src/initial.png')
-
-# create_menu.delete_menu('richmenu-d6014594d7fa7cffece808da7f901766')
