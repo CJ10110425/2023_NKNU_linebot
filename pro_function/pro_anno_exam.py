555bafbbf9b2f3342226b3d6b3f7782eadd4a805
李品澤 <ze0966747312@gmail.com>
2023-11-15 03:06:34 +0800

    change pro_function file name into snake case

---
+++ pro_function/pro_anno_exam.py
@@ -0,0 +1,58 @@
+import openai
+import re
+import os
+from dotenv import load_dotenv
+
+
+# Organize professor's copywriting
+def Organising_Profes_test_text(msg) -> (str, bool):
+    completion = openai.ChatCompletion.create(
+        model="ft:gpt-3.5-turbo-0613:personal::8Hwz1kxq",
+        messages=[{"role": "system", "content": "2023年,當一個高雄師範大學電子工程系教授的助理,幫教授整理文案並且遵照順序列出日期、時間、地點、科目、範圍（章）、是否開書考,只能回覆考試內容"}, {
+            "role": "user", "content": msg}]
+    )
+    generated_text = completion['choices'][0]['message']['content']
+    generated_text = stop_content(generated_text)
+    generated_text, flag = extract_pending_info(generated_text)
+    return generated_text, flag
+
+
+# Filter pending items and remind users to complete them
+def extract_pending_info(text) -> str:
+    pattern = r'-(\s*\S+?)\s*：\s*待定'
+    matches = re.findall(pattern, text)
+
+    if matches:
+        pending_items = '、'.join(matches).strip()
+        return f"\n請完成『{pending_items}』項目", False
+    elif is_hyphen_starting(text):
+        return text, True
+    else:
+        return "\n"+text, False
+
+
+# Stop_content function
+def stop_content(text) -> str:
+    stop_word = " END"
+    if stop_word in text:
+        generated_text = text.split(stop_word)[0].strip()
+        return generated_text
+    else:
+        return text
+
+# confirm hyphen starting
+
+
+def is_hyphen_starting(input_str):
+    if input_str.startswith('-'):
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    # loading evn var
+    load_dotenv()
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    msg = "上午11點到下午2點考物理第三章到第八章在512教室不開書考試"
+    print(Organising_Profes_test_text(msg))
---
+++ pro_function/pro_functions.py
@@ -0,0 +1,142 @@
+'''
+    professor functions
+    1.distribute_homework
+    2.announce_exam
+    3.handle_student_question
+'''
+import os
+from dotenv import load_dotenv
+import openai
+import pro_anno_exam
+
+
+class professor:
+    def __init__(self, linebot) -> None:
+        load_dotenv()
+        self.linebot = linebot
+        self.openai_api_key = os.getenv("OPENAI_API_KEY")
+        openai.api_key = self.openai_api_key
+        self.reply_msg = ""
+
+    def professor_function(self) -> None:
+        Status = self.linebot.Status.lower()
+        match Status:
+            case "standard":
+                self.determine_function()
+            case "distribute_homework":
+                self.distribute_homework()
+            case "announce_exam":
+                self.announce_exam()
+            case "handle_student_question":
+                self.handle_student_question()
+
+    def determine_function(self) -> None:
+        match self.linebot.msg:
+            case "distribute_homework":
+                self.linebot.Change_Status("distribute_homework")
+                self.distribute_homework()
+            case "announce_exam":
+                self.linebot.Change_Status("announce_exam")
+                self.announce_exam()
+            case "handle_student_question":
+                self.linebot.Change_Status("handle_student_question")
+                self.handle_student_question()
+
+    def announce_exam_level(self) -> int:
+        '''
+            to check msg user inputed correctness
+        '''
+
+        msg = str(self.linebot.msg)
+        level = self.linebot.Level
+        match level:
+            case 0:
+                if msg.lower() == "announce_exam":
+                    return level
+                else:
+                    self.reply_msg, flag = pro_anno_exam.Organising_Profes_test_text(
+                        self.linebot.msg)
+                    if flag:
+                        return level + 1
+                    else:
+                        return level
+            case 1:
+                if msg.lower() == "ok":
+                    return level + 1
+                else:
+                    return level - 1
+            case _:
+                print("error")
+
+    def announce_exam(self) -> None:
+        '''TODO: create rule.json to set up the rule (read/write file)'''
+        '''TODO: create get_rule.py to read rule.json'''
+        level = self.announce_exam_level()
+        match level:
+            case 0:
+                self.linebot.Change_Level(level)
+                # TODO: create rule.json to set up the rule (read/write file)
+                self.linebot.reply_msg("rule"+self.reply_msg)
+            case 1:
+                self.linebot.Change_Level(level)
+                self.linebot.reply_msg(
+                    self.reply_msg+"\nplease confirm whether the text is correct (ok/no)")
+            case 2:
+                self.linebot.Change_Level(0)
+                self.linebot.Change_Status("Standard")
+                '''
+                    TODO: send msg to student
+                '''
+                self.linebot.reply_msg(
+                    "completed sending message to your students,thank you")
+
+    def distribute_homework_level(self) -> int:
+        '''
+            to check msg user inputed correctness
+        '''
+
+        msg = str(self.linebot.msg)
+        level = self.linebot.Level
+        match level:
+            case 0:
+                if msg.lower() == "distribute_homework":
+                    return level
+                else:
+                    self.reply_msg, flag = pro_anno_exam.Organising_Profes_test_text(  # TODO: create Pro_dist_work.Organising_Profes_test_text
+                        self.linebot.msg)
+            case 1:
+                if msg.lower() == "ok":
+                    return level + 1
+                else:
+                    return level - 1
+            case _:
+                print("error")
+
+    def distribute_homework(self) -> None:
+        '''TODO: create rule.json to set up the rule (read/write file)'''
+        '''TODO: create get_rule.py to read rule.json'''
+        level = self.distribute_homework_level()
+        match level:
+            case 0:
+                self.linebot.Change_Level(level)
+                self.linebot.reply_msg("rule"+self.reply_msg)
+            case 1:
+                self.linebot.Change_Level(level)
+                self.linebot.reply_msg(
+                    self.reply_msg+"\nplease confirm whether the text is correct (ok/no)")
+            case 2:
+                self.linebot.Change_Level(0)
+                self.linebot.Change_Status("Standard")
+                '''
+                    TODO: send msg to student
+                '''
+                self.linebot.reply_msg(
+                    "completed sending message to your students,thank you")
+                self.linebot.reply_msg(
+                    "completed sending message to your students,thank you")
+
+    def handle_student_question(self) -> None:
+        '''
+            use to handle student question
+        '''
+        pass
