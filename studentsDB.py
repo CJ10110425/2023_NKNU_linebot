1a10fe4f3ab2ce02101bb4047e299cc0275652ad
SantaChris <christmastime20001225@gmail.com>
2023-11-18 22:44:38 +0800

    first function

--- studentsDB.py
+++ studentsDB.py
@@ -18,10 +18,13 @@ def insert_userprofile(student_id, student_gender, student_name, student_class):
                      "Student_name": student_name, "student_class": student_class})
     ## 學生課程要用集合表示 ##
 
-    ## 從學生的課程找尋學生的姓名 ##
+    
 
 
 def class_to_name(target_student_class):
+    
+    ## 從學生的課程找尋學生的姓名 ##
+    
     classes = []
     ## 鎖定想查詢課程 ##
     query = {"student_class": {"$in": [target_student_class]}}
@@ -32,10 +35,13 @@ def class_to_name(target_student_class):
         classes.append(result["Student_name"])
     return classes
 
-    ## 從學生的課程找尋學生的ID ##
+    
 
 
 def class_to_id(target_student_class):
+    
+    ## 從學生的課程找尋學生的ID ##
+    
     IDs = []
     ## 鎖定想查詢課程##
     query = {"student_class": {"$in": [target_student_class]}}
