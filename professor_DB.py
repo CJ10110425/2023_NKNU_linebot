3ac6f0530ab55e47809ee2d39084e1b6579216e1
jason-50302911 <s50302911@gamil.com>
2023-11-22 12:53:11 +0800

    temporary commit

--- professor_DB.py
+++ professor_DB.py
@@ -15,14 +15,26 @@ with open(validation_path, "r") as file:
     json_and_doc = json.load(file)
 
 validation_rules = json_and_doc["validation_rules"]
-doc = json_and_doc["document"]["insert_test"]
+test_doc = json_and_doc["document"]["insert_test"]
 
 client = pymongo.MongoClient(os.getenv("MONGODB_URL"))
-nknu_linebot_db = client["NKNU_Database"]
-professor_db = nknu_linebot_db.create_collection("professor_db", validation=validation_rules)
+nknu_linebot_db = client["NKNU_DataBase"]
 
 
-def create_professor_info(data: dict) -> None:
+def create_professor_database(db_name: str) -> pymongo.CursorType:
+    
+    collection_list = nknu_linebot_db.list_collection_names()
+    
+    if db_name not in collection_list:
+        professor_db = nknu_linebot_db.create_collection(db_name, validator=validation_rules)
+    else:
+        professor_db = nknu_linebot_db.find(db_name)
+
+    return professor_db
+
+
+def create_professor_info(db_name: pymongo.CursorType,
+                          data: dict) -> None:
     
     """
     This function create collections and inserting data.
@@ -35,20 +47,20 @@ def create_professor_info(data: dict) -> None:
                 "Gender": "male",
                 "Subject_Taught": "English"}
     """
-    professor_info = professor_db.find({"name": data["name"]})
+    professor_info = db_name.find({"name": data["name"]})
     
     if professor_info is None:
         try:
-            professor_db.insert_one(data) 
+            db_name.insert_one(data) 
+            print("[INFO] Insert successful")
         except BulkWriteError as e:
             print(f"ValidationError: {e.details['writeEorrors'][0]['errmsg']}")
     
 
-def delete_data(delete_target: str) -> None:
+def delete_data(db_name: pymongo.CursorType,
+                delete_name: str) -> None:
     
-    collection = professor_db["subject_taught"]
-    
-    collection.delete_one(delete_target)
+    db_name.delete_one({"name": delete_name})
     
     
 def update_data(professor: str,
@@ -78,12 +90,16 @@ def find_data(professor: str,
     except ValueError:
         print(f"This data is not in the collection")
     
+
+def add_data(db_name: pymongo.CursorType,
+             add_data: dict) -> pymongo.CursorType: 
     
-    
+    return db_name.insert_one(add_data)
     
     
 if __name__ == '__main__':     
     
     print("[INFO] Test is successful")
-    insert_data(doc)
     
+    test_DB = create_professor_database("professor_database")
+    print(test_DB)
